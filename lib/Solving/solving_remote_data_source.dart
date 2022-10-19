55c45789b402228366b66e7cef719b2c4531717c
Konrad Piszczek <kpiszczek67@gmail.com>
2022-10-19 21:12:45 +0200

    Add Dio error handling

--- lib/Solving/solving_remote_data_source.dart
+++ lib/Solving/solving_remote_data_source.dart
@@ -2,8 +2,13 @@ import 'package:dio/dio.dart';
 
 class SolvingRemoteDataSource {
   Future<Map<String, dynamic>?> getSolvingData() async {
-    final response = await Dio().get<Map<String, dynamic>>(
-        "https://my-json-server.typicode.com/piszczekkonrad/rubiks_timer/whiteCross");
-    return response.data;
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(
+          "https://my-json-server.typicode.com/piszczekkonrad/rubiks_timer/whiteCross");
+      return response.data;
+    } on DioError catch (error) {
+      throw Exception(
+          error.response?.data["error"]["message"] ?? "Unknown error");
+    }
   }
 }
