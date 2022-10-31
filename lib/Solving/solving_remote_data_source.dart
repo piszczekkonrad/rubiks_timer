c7b550d7794a4cdb65889aeb06660e25a8f169e0
Konrad Piszczek <kpiszczek67@gmail.com>
2022-10-31 17:21:50 +0100

    Manage conflicts after merging

--- lib/Solving/solving_remote_data_source.dart
+++ lib/Solving/solving_remote_data_source.dart
@@ -22,16 +22,4 @@ abstract class SolvingRemoteRetrofitDataSource {
   Future<List<SolvingModel>> getPLLData();
   @GET("/OrientLastLayer")
   Future<List<SolvingModel>> getOLLData();
-
-
-class SolvingRemoteDioDataSource {
-  Future<List<Map<String, dynamic>>?> getSolvingData() async {
-    final response = await Dio().get<List<dynamic>>(
-        "https://my-json-server.typicode.com/piszczekkonrad/rubiks_timer/OrientLastLayer");
-    final listDynamic = response.data;
-    if (listDynamic == null) {
-      return null;
-    }
-    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-  }
 }
