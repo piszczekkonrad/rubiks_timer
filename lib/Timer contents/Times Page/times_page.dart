495e16a0850b3ffa805112d17ade9b55fee44ff4
Konrad Piszczek <kpiszczek67@gmail.com>
2023-06-28 20:38:31 +0200

    Make widgets smaller

--- lib/Timer contents/Times Page/times_page.dart
+++ lib/Timer contents/Times Page/times_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rubiks_timer/Timer%20contents/Root/cubit/timer_root_cubit.dart';
 import 'package:rubiks_timer/Timer%20contents/Root/timer_root_navigation_bar.dart';
+import 'package:rubiks_timer/Timer%20contents/Times%20Page/times_model.dart';
 import 'package:rubiks_timer/injection_container.dart';
 
 import 'cubit/times_page_cubit.dart';
@@ -57,75 +58,128 @@ class TimesPageContent extends StatelessWidget {
               for (final timeModel in timeModels) ...[
                 Padding(
                   padding: const EdgeInsets.all(10.0),
-                  child: Dismissible(
-                    background: const DecoratedBox(
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                      ),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Padding(
-                          padding: EdgeInsets.only(right: 32.0),
-                          child: Icon(
-                            Icons.delete,
-                          ),
-                        ),
-                      ),
-                    ),
-                    confirmDismiss: (direction) async {
-                      return direction == DismissDirection.endToStart;
-                    },
-                    key: ValueKey(timeModel.id),
-                    onDismissed: (_) {
-                      context.read<TimesPageCubit>().delete(timeModel.id);
-                    },
-                    child: Container(
-                      color: Colors.green,
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            color: Colors.white,
-                            padding: const EdgeInsets.all(2),
-                            margin: const EdgeInsets.all(2),
-                            child: Text(
-                              (timeModels.indexOf(timeModel) + 1).toString(),
-                              style: GoogleFonts.lato(
-                                textStyle: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    letterSpacing: 1),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  timeModel.time,
-                                  style: GoogleFonts.lato(
-                                    textStyle: const TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        letterSpacing: 1),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                  child: _ListViewItem(
+                      timeModel: timeModel, timeModels: timeModels),
                 ),
               ],
             ],
           );
         },
+      ),
+    );
+  }
+}
+
+class _ListViewItem extends StatelessWidget {
+  const _ListViewItem({
+    Key? key,
+    required this.timeModel,
+    required this.timeModels,
+  }) : super(key: key);
+
+  final TimesModel timeModel;
+  final List<TimesModel> timeModels;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      background: const _ListViewItemBackgroud(),
+      confirmDismiss: (direction) async {
+        return direction == DismissDirection.endToStart;
+      },
+      key: ValueKey(timeModel.id),
+      onDismissed: (_) {
+        context.read<TimesPageCubit>().delete(timeModel.id);
+      },
+      child: Container(
+        color: Colors.green,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _ListViewItemIndex(timeModels: timeModels, timeModel: timeModel),
+            _ListViewItemText(timeModel: timeModel),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ListViewItemText extends StatelessWidget {
+  const _ListViewItemText({
+    Key? key,
+    required this.timeModel,
+  }) : super(key: key);
+
+  final TimesModel timeModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            timeModel.time,
+            style: GoogleFonts.lato(
+              textStyle: const TextStyle(
+                  fontSize: 20, fontWeight: FontWeight.bold, letterSpacing: 1),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ListViewItemIndex extends StatelessWidget {
+  const _ListViewItemIndex({
+    Key? key,
+    required this.timeModels,
+    required this.timeModel,
+  }) : super(key: key);
+
+  final List<TimesModel> timeModels;
+  final TimesModel timeModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(2),
+      margin: const EdgeInsets.all(2),
+      child: Text(
+        (timeModels.indexOf(timeModel) + 1).toString(),
+        style: GoogleFonts.lato(
+          textStyle: const TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, letterSpacing: 1),
+        ),
+      ),
+    );
+  }
+}
+
+class _ListViewItemBackgroud extends StatelessWidget {
+  const _ListViewItemBackgroud({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.red,
+      ),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Padding(
+          padding: EdgeInsets.only(right: 32.0),
+          child: Icon(
+            Icons.delete,
+          ),
+        ),
       ),
     );
   }
--- lib/Timer contents/User Page/user_page.dart
+++ lib/Timer contents/User Page/user_page.dart
@@ -26,47 +26,70 @@ class UserPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'You are logged in as:\n ${user.email}',
-                style: GoogleFonts.lato(
-                  textStyle: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green,
-                fixedSize: const Size(240, 60),
-              ),
-              onPressed: () {
-                context.read<TimerRootCubit>().signOut();
-              },
-              child: Text(
-                'Log Out',
-                style: GoogleFonts.lato(
-                  textStyle: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: _UserPageContents(user: user),
       bottomNavigationBar: RootBottomNavigationBar(
         currentIndex: 2,
         setIndex: context.read<TimerRootCubit>().setIndex,
+      ),
+    );
+  }
+}
+
+class _UserPageContents extends StatelessWidget {
+  const _UserPageContents({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'You are logged in as:\n ${user.email}',
+              style: GoogleFonts.lato(
+                textStyle: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const _LogOutButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class _LogOutButton extends StatelessWidget {
+  const _LogOutButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.green,
+        fixedSize: const Size(240, 60),
+      ),
+      onPressed: () {
+        context.read<TimerRootCubit>().signOut();
+      },
+      child: Text(
+        'Log Out',
+        style: GoogleFonts.lato(
+          textStyle: const TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, letterSpacing: 1),
+        ),
       ),
     );
   }
